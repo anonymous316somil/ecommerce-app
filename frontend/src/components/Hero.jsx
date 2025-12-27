fcb53f82d426f122a7f1a6b15daa56ec484ba119
Soumil <150796783+anonymous316somil@users.noreply.github.com>
2025-12-27 10:04:17 +0000

    Add LatestCollection and ShopContext

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {assets} from '../assets/assets'
+import { assets } from '../assets/assets'
 
 const Hero = () => {
     return (
@@ -13,8 +13,8 @@ const Hero = () => {
                     </div>
                     <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
                     <div className='flex items-center gap-2'>
-                    <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
-                    <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+                        <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
+                        <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
                     </div>
                 </div>
             </div>
