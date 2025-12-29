8a5244a3f790b5eeb2e0027fcad41a0956888071
Soumil <150796783+anonymous316somil@users.noreply.github.com>
2025-12-29 11:34:21 +0000

    Add Place Order page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
             <div>
                 <hr />
                 <p className='py-5 text-sm text-center'>
-                    Copyright 2025@ buykaro.com - All Right Reserved.
+                    Copyright 2025@ karobuy.com - All Right Reserved.
                 </p>
             </div>
         </div>
