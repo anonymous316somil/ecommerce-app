398d3e63ea8c4dc6617d7179f5c3bf0311c55522
Soumil <150796783+anonymous316somil@users.noreply.github.com>
2025-12-30 10:27:00 +0000

    Add Product Upload Feature

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
+import productRouter from "./routes/productRoute.js"
 
 // App Config
 const app = express()
@@ -17,6 +18,7 @@ app.use(cors())
 
 // api endpoints
 app.use('/api/user', userRouter)
+app.use('/api/product', productRouter); 
 
 app.get('/', (req, res) => {
     res.send("API Working")
@@ -24,4 +26,4 @@ app.get('/', (req, res) => {
  
 app.listen(port, () => console.log('Server started on PORT : ' + port))
  
-        
+         
